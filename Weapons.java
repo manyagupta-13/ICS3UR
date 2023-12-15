a15288432b3f70f576273fd5b90d5c401a16d032
enitaity <153942703+enitaity@users.noreply.github.com>
2023-12-15 13:00:52 -0500

    fixed file upload

---
+++ Weapons.java
@@ -0,0 +1,15 @@
+public class Weapons { 
+  private double damageMod; //damage modifier
+  private int power; //base power
+  private double speedMod; // speed modifier
+  // private img displayImg;
+  // private projectile proj;
+
+  public Weapons(double dmg, int power, double speed){
+    this.damageMod=dmg;
+    this.power=power;
+    this.speedMod=speed;
+  }
+
+  
+}
