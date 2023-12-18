23cc634317458b616f590926982bb639b9293ade
manyagupta-13 <148280602+manyagupta-13@users.noreply.github.com>
2023-12-17 22:49:11 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,15 +2,26 @@ import javax.swing.ImageIcon;
 import java.awt.Image;
 
 public class Player{
+  //Player's character choice
   private String character;
-
+  //Player's choise to shoot or move
   private boolean move;
   private boolean shoot;
+  //Player's helth
   private int health;
-  private Weapon weapon;
+  //Coordinates of top left corner of character image
   private int xPos;
   private int yPos;
-  private Hitbox hitbox;
+  //Width and height of character image
+  private final static int width;
+  private final static int height;
+  //Objects to determine health and other attributed
+  private Weapon weapon;
+  private Projectile projectile;
+  //Object to determine width and height of character image
+  private BufferedImage size = ImageIO(new File(//fileName));
+  //Object to determine collision
+  private Hitbox hitbox = new Hitbox(getCoordianteX(), getCoordinateY(), getWidth(), getHeight());
 
   //Image for Player GUI Representation
   private Image playerIcon;
@@ -40,6 +51,7 @@ public class Player{
     }
   }
 
+  //Return boolean if player decides to move
   public void setMove(){
     if(move button clicked){
       move = true;
@@ -49,6 +61,7 @@ public class Player{
     }
   }
 
+  //Return boolean if player decides to shoot
   public void setShoot(){
     if(shoot button clicked){
       shoot = true;
@@ -59,6 +72,7 @@ public class Player{
     }
   }
 
+  //Reduce health if player gets hit
   public void setHealth(){
     if(hitbox.intersects() == true){
       if(player.getCharacter() == "cannon"){
@@ -70,6 +84,7 @@ public class Player{
     }
   }
 
+  //Set top left coordinates of image character
   public void setCoordinateY(int y){ 
       yPos = y;
   }
@@ -78,24 +93,47 @@ public class Player{
         xPos = x;
   }
 
+  //Set width and height of image character
+  public void setWidth(){
+    width = size.getWidth();
+  }
+
+  public void setHeight(){
+    height = size.getHeight();
+  }
+
+  //Return character chosen by player
   public String getCharacter(){
     return character;
   }
 
+  //Return if player decides to move
   public boolean getMove(){
     return move;
   }
 
+  //Return if player decides to shoot
   public boolean getShoot(){
     return shoot;
   }
 
+  //Return player's health
   public int getHealth(){
     return health;
   }
 
+  //Return character image
   public Image getImage() {
     return image;
+  }
+
+  //Return top left coordinates of image character
+  public int getCoordinateX(){
+    return xPos;
+  }
+
+  public int getCoordinateY(){
+    return yPos;
   }
 
   //Return width and height of image character
