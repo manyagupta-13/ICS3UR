60062471fbf154fb68e4c45a02814f16c2f3be0a
enitiaty <jimmysjzhang@gmail.com>
2023-12-19 13:18:06 -0500

    weapons changed to be more complete

--- Main/src/Weapons.java
+++ Main/src/Weapons.java
@@ -8,7 +8,8 @@ public class Weapons {
   // private int power; //base power
   private double speedMod; // speed modifier
   private Image displayImg;
-  private Projectile proj;
+  private Projectile proj = new Projectile();
+  private int x,y;
   // TODO: scale mx,my to fit screen
   private double v0, theta, t, range;
 
@@ -20,10 +21,16 @@ public class Weapons {
     this.displayImg=img;
   }
 
-  public Weapons(String weapon){
-
+  public double getDmg(){
+    return proj.getDamage();
   }
-  public void setCoords(int mx, int my){
+  public void setWeaponCoords(int x, int y){
+    this.x=x;
+    this.y=y;
+  }
+
+
+  public void setMouseCoords(int mx, int my){
     theta = -Math.atan(my*my/mx/mx);
     v0=Math.sqrt(mx*mx+my*my);
     t=(2*v0*Math.sin(theta))/G;
@@ -35,25 +42,33 @@ public class Weapons {
     return this.speedMod;
   }
 
-  
+  public double getFlightTime(){
+    return t;
+  }
 
   
   public double[][] getPathShort(){
-    double[][] ret = new double[2][3];
+    double[][] ret = new double[2][20];
     for (int i = 1; i < 4; i++){
       double temp = (range/10)*i;
-      ret[0][i-1]=temp;
-      ret[1][i-1]=temp*Math.tan(theta)-(G*temp*temp/(2*v0*v0*Math.cos(theta)*Math.cos(theta)));
+      ret[0][i-1]=x+temp;
+      ret[1][i-1]=y+temp*Math.tan(theta)-(G*temp*temp/(2*v0*v0*Math.cos(theta)*Math.cos(theta)));
     }
     return ret;
   }
   
   public double[][] getPathFull(){
-    double[][] ret = new double[2][20];
+    double[][] ret = new double[3][20];
     for (int i = 1; i < 21; i++){
       double temp = (range/20)*i;
-      ret[0][i-1]=temp;
-      ret[1][i-1]=temp*Math.tan(theta)-(G*temp*temp/(2*v0*v0*Math.cos(theta)*Math.cos(theta)));
+      ret[0][i-1]=x+temp;
+      ret[1][i-1]=y+temp*Math.tan(theta)-(G*temp*temp/(2*v0*v0*Math.cos(theta)*Math.cos(theta)));
+      ret[2][i-3]=180*Math.atan(
+        Math.tan(theta)-
+        (2*temp*G)
+        /
+        (2*v0*v0*Math.pow(Math.cos(theta),2))
+      )/(2*Math.PI);
     }
     return ret;
   }
